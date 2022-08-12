f8a75f6abce246aae40f0b11be2128f21947d50f
Alexander Ryakhin <bonney977@gmail.com>
2022-08-12 08:58:54 +0200

    Added a couple of screens

---
+++ Billie Eilish/BillieEilishApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillieEilishApp.swift
+//  BillieEilish
+//
+//  Created by Alexander Ryakhin on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BillieEilishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
