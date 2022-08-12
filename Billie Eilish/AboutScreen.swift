c0d63ab3445aabd7239ece4575847b2a55f7cdc7
Alexander Ryakhin <bonney977@gmail.com>
2022-08-12 08:58:54 +0200

    Added a couple of screens

---
+++ Billie Eilish/AboutScreen.swift
@@ -0,0 +1,25 @@
+//
+//  AboutScreen.swift
+//  Billie Eilish
+//
+//  Created by Alexander Ryakhin on 8/12/22.
+//
+
+import SwiftUI
+
+struct AboutScreen: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+            }
+            .navigationTitle("About")
+        }
+    }
+}
+
+struct AboutScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutScreen()
+    }
+}
